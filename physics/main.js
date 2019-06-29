f3e28d460b9a3b13da60aea64abf715cbacd0c80
tortshwar <a>
2019-06-29 13:25:34 -0700

    a

--- physics/main.js
+++ physics/main.js
@@ -5,34 +5,29 @@ var discs = [],
   restitution = 0.675;
 
 discs.push({
-  x: 210,
-  y: 245,
-  oldx: 200,
-  oldy: 240,
-  r: 15,
+  x: 300,
+  y: 100,
+  velocity: {
+    x: 0,
+    y: 0
+  },
+  mass: 1,
+  radius: 15,
   fill: 'rgb(0,0,255)',
   options: {
     
   }
 });
 discs.push({
-  x: 200,
-  y: 50,
-  oldx: 215,
-  oldy: 57,
-  r: 10,
+  x: 100,
+  y: 100,
+  velocity: {
+    x: 2,
+    y: 0
+  },
+  mass: 2,
+  radius: 30,
   fill: 'rgb(255,255,0)',
-  options: {
-    
-  }
-});
-discs.push({
-  x: 175,
-  y: 10,
-  oldx: 160,
-  oldy: 10,
-  r: 5,
-  fill: 'rgb(255,0,0)',
   options: {
     
   }
@@ -44,129 +39,7 @@ function setup() {
 
 function draw() {
   updateDiscs();
-  checkCircleCollision();
-  constrainDiscs();
+  solveConstraints();
+  solveCollisions();
   renderDiscs();
-}
-
-function mouseReleased() {
-  addDisc();
-}
-
-function addDisc() {
-  discs.push({
-    x: mouseX,
-    y: mouseY,
-    oldx: pmouseX,
-    oldy: pmouseY,
-    r: 10,
-    fill: '#000',
-    options: {
-      
-    }
-  });
-}
-
-function updateDiscs() {
-  for(var i=0; i < discs.length; i++) {
-    var d = discs[i];
-    if(d.options.freeze !== true) {
-      vx = d.x - d.oldx,
-        vy = d.y - d.oldy;
-      
-      d.oldx = d.x;
-      d.oldy = d.y;
-      d.x += vx;
-      d.y += vy;
-      d.y += gravity;
-    }
-  }
-}
-
-function constrainDiscs() {
-  for(var i=0; i < discs.length; i++) {
-    var d = discs[i];
-    
-    if(d.options.freeze !== true) {
-      vx = d.x - d.oldx,
-        vy = d.y - d.oldy;
-      
-      //boundary collisions
-      if(d.x+d.r > width) {
-        d.x = width-d.r;
-        d.oldx = d.x + vx * restitution;
-      }
-      if(d.x-d.r < 0) {
-        d.x = 0+d.r;
-        d.oldx = d.x + vx * restitution;
-      }
-      if(d.y+d.r > height) {
-        d.y = height-d.r;
-        d.oldy = d.y + vy *  restitution;
-      }
-      if(d.y-d.r < 0) {
-        d.y = 0+d.r;
-        d.oldy = d.y + vy * restitution;
-      }
-    }
-  }
-}
-
-
-/*function solveCollision(a, b) {
-  var pointOfContactX = (a.r*a.x + b.r*b.x)/(a.r+b.r),
-    pointOfContactY = (a.r*a.y + b.r*b.y)/(a.r+b.r);
-  
-  var n = 0;
-  
-  if(n === 0) {
-    discs.push({
-      x: pointOfContactX,
-      y: pointOfContactY,
-      oldx: pointOfContactX,
-      oldy: pointOfContactY,
-      r: 5,
-      fill: 0,
-      options: {
-        freeze: 1
-      }
-    });
-    n = 1;
-  }
-  
-  ellipse(pointOfContactX, pointOfContactY, 100);
-}*/
-
-function checkCircleCollision() {
-  for(var i=0; i < discs.length; i++) {
-    for(var j=0; j < discs.length; j++) {
-      var d0 = discs[i];
-      var d1 = discs[j];
-      if(i == j) {
-        continue;
-      }
-      var distance = Math.pow(d1.x-d0.x, 2)+Math.pow(d1.y-d0.y, 2);
-      if(distance <= Math.pow(d0.r+d1.r, 2)) {
-        //solveCollision(d0, d1);
-        d0.options.freeze = true;
-        d1.options.freeze = true;
-        
-        var angle = -Math.atan2(d1.y - d0.y, d1.x - d0.x);
-        
-      }
-    }
-  }
-}
-
-function renderDiscs() {
-  background(bg);
-  for(var i=0; i < discs.length; i++) {
-    var d = discs[i];
-    fill(d.fill);
-    ellipse(d.x, d.y, d.r*2);
-  }
-}
-
-function magnitude(v) {
-  return Math.sqrt(Math.pow(v.x - v.oldx, 2)+Math.pow(v.y - v.oldy, 2));
 }
--- physics/utility.js
+++ physics/utility.js
@@ -1,8 +1,89 @@
-function rotate(velocity, angle) {
-  const rotatedVelocities = {
-    x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
-    y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
-  };
-  
-  return rotatedVelocities;
+function updateDiscs() {
+  for(var i=0; i < discs.length; i++) {
+    var d = discs[i];
+    if(d.options.freeze !== true) {
+      d.x += d.velocity.x;
+      d.y += d.velocity.y;
+      d.velocity.y += gravity;
+    }
+  }
+}
+
+function solveConstraints() {
+  for(var i=0; i < discs.length; i++) {
+    var d = discs[i];
+    
+    if(d.options.freeze !== true) {
+      var radius = d.radius;
+      
+      //boundary collisions
+      if(d.x+radius > width) {
+        d.x = width - radius;
+        d.velocity.x = -d.velocity.x * restitution;
+      }
+      if(d.x-radius < 0) {
+        d.x = 0 + radius;
+        d.velocity.x = -d.velocity.x * restitution;
+      }
+      if(d.y+radius > height) {
+        d.y = height - radius;
+        d.velocity.y = -d.velocity.y * restitution;
+      }
+      if(d.y-radius < 0) {
+        d.y = 0 + radius;
+        d.velocity.y = -d.velocity.y * restitution;
+      }
+    }
+  }
+}
+
+
+function solveCollisions() {
+  for(var i=0; i < discs.length; i++) {
+    for(var j=0; j < discs.length; j++) {
+      var d0 = discs[i];
+      var d1 = discs[j];
+      if(i == j) {
+        continue;
+      }
+      var distance = Math.pow(d1.x-d0.x, 2)+Math.pow(d1.y-d0.y, 2);
+      
+      if (distance <= (d0.radius+d1.radius)*(d0.radius+d1.radius)) {
+        
+        var theta1 = Math.atan2(d0.y - d0.velocity.y, d0.x - d0.velocity.x);
+        var theta2 = Math.atan2(d1.y - d1.velocity.y, d1.x - d1.velocity.x);
+        var phi = Math.atan2(d1.y - d0.y, d1.x - d0.x);
+        
+        var m1 = d0.mass;
+        var m2 = d1.mass;
+        
+        var v1 = Math.sqrt(Math.pow(d0.x-d0.velocity.x, 2)+Math.pow(d0.y - d0.velocity.y, 2));
+        var v2 = Math.sqrt(Math.pow(d1.x-d1.velocity.x, 2)+Math.pow(d1.y - d1.velocity.y, 2));
+        
+        v1PrimeX = ((v1 * Math.cos(theta1 - phi) * (m1 - m2) + 2 * m2 * v2 * Math.cos(theta2 - phi))/(m1 + m2))*(Math.cos(phi) + v1 * Math.sin(theta1 - phi) * Math.cos(phi + (Math.PI / 2)));
+        v1PrimeY = ((v1 * Math.cos(theta1 - phi) * (m1 - m2) + 2 * m2 * v2 * Math.cos(theta2 - phi))/(m1 + m2))*(Math.sin(phi) + v1 * Math.sin(theta1 - phi) * Math.sin(phi + (Math.PI / 2)));
+        v2PrimeX = ((v2 * Math.cos(theta2 - phi) * (m2 - m1) + 2 * m1 * v1 * Math.cos(theta1 - phi))/(m1 + m2))*(Math.cos(phi) + v2 * Math.sin(theta2 - phi) * Math.cos(phi + (Math.PI / 2)));
+        v2PrimeY = ((v2 * Math.cos(theta2 - phi) * (m2 - m1) + 2 * m1 * v1 * Math.cos(theta1 - phi))/(m1 + m2))*(Math.sin(phi) + v2 * Math.sin(theta2 - phi) * Math.sin(phi + (Math.PI / 2)));
+        
+        d0.velocity.x = v1PrimeX;
+        d0.velocity.y = v1PrimeY;
+        
+        d1.velocity.x = v2PrimeX;
+        d1.velocity.y = v2PrimeY;
+      }
+    }
+  }
+}
+
+function renderDiscs() {
+  //background(bg);
+  for(var i=0; i < discs.length; i++) {
+    var d = discs[i];
+    fill(d.fill);
+    ellipse(d.x, d.y, d.radius*2);
+  }
+}
+
+function magnitude(v) {
+  return Math.sqrt(Math.pow(v.x - v.oldx, 2)+Math.pow(v.y - v.oldy, 2));
 }
